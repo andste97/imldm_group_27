b9cbb0f8d8750105df97aa2998fbaba172defbb7
Andreas Stehle <andreas.stehle@student.tuwien.ac.at>
2022-03-01 16:53:33 +0100

    fix errors

--- project_1_scripts/project_1_code.py
+++ project_1_scripts/project_1_code.py
@@ -8,23 +8,28 @@ print(df)
 raw_data = df.values
 print(raw_data)
 
-cols = range (1,8) # exclude column 0 because
+cols = range (1,11) # exclude column 0 because
   # row numbers are not needed as attributes
 X = raw_data[:, cols]
 print(X)
 
-attribute_names = np.asarray(df.columns[cols])
-print(attribute_names)
+attributeNames = np.asarray(df.columns[cols])
+print(attributeNames)
 
-classLabels = raw_data[:, 5]
+classLabels = X[:, 4]
 classNames = np.unique(classLabels)
 classDict = dict(zip(classNames, range(len(classNames))))
 # column 5 transformed from text present/absent to 1/0
 v_famhist_transformed = np.array([classDict[value] for value in classLabels])
-print('Vector famhist: ', v_famhist_transformed)
+print('Vector famhist transformed: ', v_famhist_transformed)
 
-X[:, -1] = v_famhist_transformed
-print("X, last column replaced with transformed values: ", X)
+X[:, 4] = v_famhist_transformed
+print("X, column 4 replaced with transformed values: \n", X)
+
+N = len(raw_data[:, -1])
+M = len(attributeNames)
+C = len(classNames)
 
 # Subtract mean value from data
 X_centered = X - np.ones((N,1))*X.mean(axis=0)
+print("X, centered: ", X_centered)
