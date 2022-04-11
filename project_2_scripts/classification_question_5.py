159d7d6e6382df06383f0d0a3e71485f8b3dd29c
Andreas Stehle <andreas.stehle2@gmail.com>
2022-04-11 16:13:40 +0200

    add code for question 5

--- project_2_scripts/classification_question_5.py
+++ project_2_scripts/classification_question_5.py
@@ -1,20 +1,15 @@
 import numpy as np
 import pandas as pd
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
 
 url = "https://hastie.su.domains/ElemStatLearn/datasets/SAheart.data"
 df = pd.read_csv(url)
-print(df)
-
 raw_data = df.values
-print(raw_data)
-
 cols = range(1, 11)  # exclude column 0 because
 # row numbers are not needed as attributes
 X = raw_data[:, cols]
-print(X)
-
 attributeNames = np.asarray(df.columns[cols])
-print(attributeNames)
 
 classLabels = X[:, 9]
 classNames = np.unique(classLabels)
@@ -39,5 +34,32 @@ N = len(raw_data[:, -1])
 M = len(attributeNames)
 C = len(classNames)
 
+# convert to float
 X_float = np.array(X, dtype=np.float64)
 
+#standardize data
+mu = np.mean(X_float, 0)
+sigma = np.std(X_float, 0)
+X_standardized = (X_float - mu) / sigma
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X_standardized, y, test_size=0.3)
+
+var_lambda = 9.5
+mdl = LogisticRegression(penalty='l2', C=1 / var_lambda)
+mdl.fit(X_train, y_train)
+
+y_train_est = mdl.predict(X_train).T
+y_test_est = mdl.predict(X_test).T
+y_est_prob = mdl.predict_proba(X_test)
+y_est = np.argmax(y_est_prob, 1)
+errors = np.sum(y_est != y_test, dtype=float) / y_test.shape[0]
+
+train_error_rate = np.sum(y_train_est != y_train) / len(y_train)
+test_error_rate = np.sum(y_test_est != y_test) / len(y_test)
+
+w_est = mdl.coef_[0]
+#coefficient_norm = np.sqrt(np.sum(w_est ** 2))
+
+print("Results for training for question 5: ")
+print("train error rate: {0}, test error rate: {1}".format(train_error_rate, test_error_rate))
+print("model coefficients: {0}".format(w_est))
