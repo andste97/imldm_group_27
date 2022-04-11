bb582d5cc3db5d323413a2b0c7fc5bc41841dd2c
Andreas Stehle <andreas.stehle2@gmail.com>
2022-04-11 16:17:01 +0200

    remove train/test split

--- project_2_scripts/classification_question_5.py
+++ project_2_scripts/classification_question_5.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn import model_selection
 from sklearn.linear_model import LogisticRegression
+
+# this script trains a linear regression model on the entire CHD dataset. No train/test split is made
+# to make comparison to the linear regression model easier.
 
 url = "https://hastie.su.domains/ElemStatLearn/datasets/SAheart.data"
 df = pd.read_csv(url)
@@ -42,24 +44,27 @@ mu = np.mean(X_float, 0)
 sigma = np.std(X_float, 0)
 X_standardized = (X_float - mu) / sigma
 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X_standardized, y, test_size=0.3)
+X_train = X_standardized
+y_train = y
+
+#X_train, X_test, y_train, y_test = model_selection.train_test_split(X_standardized, y, test_size=0.3)
 
 var_lambda = 9.5
 mdl = LogisticRegression(penalty='l2', C=1 / var_lambda)
 mdl.fit(X_train, y_train)
 
 y_train_est = mdl.predict(X_train).T
-y_test_est = mdl.predict(X_test).T
-y_est_prob = mdl.predict_proba(X_test)
-y_est = np.argmax(y_est_prob, 1)
-errors = np.sum(y_est != y_test, dtype=float) / y_test.shape[0]
+#y_test_est = mdl.predict(X_test).T
+#y_est_prob = mdl.predict_proba(X_test)
+#y_est = np.argmax(y_est_prob, 1)
+#errors = np.sum(y_est != y_test, dtype=float) / y_test.shape[0]
 
 train_error_rate = np.sum(y_train_est != y_train) / len(y_train)
-test_error_rate = np.sum(y_test_est != y_test) / len(y_test)
+#test_error_rate = np.sum(y_test_est != y_test) / len(y_test)
 
 w_est = mdl.coef_[0]
 #coefficient_norm = np.sqrt(np.sum(w_est ** 2))
 
 print("Results for training for question 5: ")
-print("train error rate: {0}, test error rate: {1}".format(train_error_rate, test_error_rate))
+print("train error rate: {0}".format(train_error_rate))
 print("model coefficients: {0}".format(w_est))
